214ffefc2b70d378036e1202fae5f0d5e3e1ffc9
Bogdan Silvasan <bogdan.silve@gmail.com>
2021-04-07 16:19:21 +0300

    Test code...

--- src/main/java/com/attasportsapp/AttasportsappApplication.java
+++ src/main/java/com/attasportsapp/AttasportsappApplication.java
@@ -1,8 +1,21 @@
 package com.attasportsapp;
 
+import com.attasportsapp.controllers.CountryController;
+import com.attasportsapp.controllers.CountyController;
+import com.attasportsapp.controllers.LocationController;
+import com.attasportsapp.models.Country;
+import com.attasportsapp.models.County;
+import com.attasportsapp.models.Location;
+import com.attasportsapp.models.Sport;
+import com.attasportsapp.repositories.CountryRepository;
+import com.attasportsapp.repositories.CountyRepository;
+import com.attasportsapp.repositories.LocationRepository;
+import com.attasportsapp.repositories.SportRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EntityScan
@@ -12,4 +25,24 @@ public class AttasportsappApplication {
         SpringApplication.run(AttasportsappApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(CountryRepository countryRepository, CountyRepository countyRepository, LocationRepository locationRepository,
+                                        SportRepository sportRepository, CountryController countryController, CountyController countyController,
+                                        LocationController locationController) {
+        return args -> {
+            Country country = new Country("Romania");
+            country = countryRepository.save(country);
+            County county = new County("Cluj", country);
+            county = countyRepository.save(county);
+            Location location = new Location("Cheile Turzii", county);
+            location = locationRepository.save(location);
+
+            countryController.getCountryJPQL(country.getCountryId()).getCounties().forEach(c -> System.out.println(c.getName()));
+            countyController.getCountyJPQL(county.getCountyId()).getLocations().forEach(l -> System.out.println(l.getName()));
+            if ((location = locationController.getLocationJPQL(location.getLocationId())) != null) { // Checking NoResultException handler
+                System.out.println("Reached here");
+                location.getSports().forEach(s -> System.out.println(s.getName()));
+            }
+        };
+    }
 }
