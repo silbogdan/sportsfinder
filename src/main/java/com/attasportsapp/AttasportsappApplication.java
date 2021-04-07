1e6ed0b1474351005af29c357713d7b9d7a896f1
Bogdan Silvasan <bogdan.silve@gmail.com>
2021-04-07 11:18:41 +0300

    Initial commit

---
+++ src/main/java/com/attasportsapp/AttasportsappApplication.java
@@ -0,0 +1,13 @@
+package com.attasportsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttasportsappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttasportsappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/attasportsapp/AttasportsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attasportsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttasportsappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
