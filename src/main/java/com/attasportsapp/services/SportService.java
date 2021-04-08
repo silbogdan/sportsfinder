328f401904eeb843d9641caf18c8fd7905f4c8ca
Bogdan Silvasan <bogdan.silve@gmail.com>
2021-04-08 18:13:59 +0300

    Modified Service with the right parameters

--- src/main/java/com/attasportsapp/services/SportService.java
+++ src/main/java/com/attasportsapp/services/SportService.java
@@ -1,6 +1,7 @@
 package com.attasportsapp.services;
 
 import com.attasportsapp.models.Sport;
+import com.attasportsapp.models.dto.SportDTO;
 import com.attasportsapp.repositories.SportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
@@ -9,16 +10,19 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class SportService {
     @Autowired
     SportRepository repository;
 
-    public List<Sport> getOrderedSports(LocalDate startDate, LocalDate endDate, String criteria) {
+    public List<Sport> getOrderedSports(List<SportDTO> givenSports, LocalDate startDate, LocalDate endDate, String criteria) {
         Sort sortOrder = Sort.by(criteria);
 
-        List<Sport> sports = repository.findAllBetweenDates(startDate, endDate, sortOrder);
+        List<String> sportsNames = givenSports.stream().map(SportDTO::getName).collect(Collectors.toList());
+
+        List<Sport> sports = repository.findAllBetweenDates(sportsNames, startDate, endDate, sortOrder);
 
         if (sports.isEmpty())
             return new ArrayList<>();
