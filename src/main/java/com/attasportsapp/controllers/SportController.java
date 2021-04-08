3b74ed44e4745c1e4dcc20d3d16d6a1980d145ec
Bogdan Silvasan <bogdan.silve@gmail.com>
2021-04-08 11:41:32 +0300

    Created an endpoint to return sorted list of sports between given dates (work in progress)

--- src/main/java/com/attasportsapp/controllers/SportController.java
+++ src/main/java/com/attasportsapp/controllers/SportController.java
@@ -1,12 +1,15 @@
 package com.attasportsapp.controllers;
 
-import com.attasportsapp.models.County;
 import com.attasportsapp.models.Sport;
 import com.attasportsapp.repositories.LocationRepository;
 import com.attasportsapp.repositories.SportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
+import com.attasportsapp.services.SportService;
 
 import javax.persistence.EntityManager;
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -15,6 +18,9 @@ public class SportController {
     private final EntityManager entityManager;
     private final SportRepository sportRepository;
     private final LocationRepository locationRepository;
+
+    @Autowired
+    SportService service;
 
     public SportController(EntityManager entityManager, SportRepository sportRepository, LocationRepository locationRepository) {
         this.entityManager = entityManager;
@@ -30,6 +36,15 @@ public class SportController {
     @GetMapping("")
     public List<Sport> getSports() {
         return sportRepository.findAll();
+    }
+
+    @GetMapping("/sorted")
+    public List<Sport> getOrderedSports( //TODO: Make Sport DTO and add it as RequestBody
+            @RequestParam(defaultValue = "cost") String sortBy,
+            @RequestParam(defaultValue = "01-01-1900") @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate startDate,
+            @RequestParam(defaultValue = "01-01-2100") @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate endDate) {
+
+        return service.getOrderedSports(startDate, endDate, sortBy);
     }
 
     @PostMapping(value = {"/{locationId}", ""})
