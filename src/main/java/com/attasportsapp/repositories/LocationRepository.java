5bfad77e5814b1385fd27d213b58358f24f439d4
Bogdan Silvasan <bogdan.silve@gmail.com>
2021-04-09 14:11:16 +0300

    Custom query to get ordered sports in location

--- src/main/java/com/attasportsapp/repositories/LocationRepository.java
+++ src/main/java/com/attasportsapp/repositories/LocationRepository.java
@@ -2,6 +2,14 @@ package com.attasportsapp.repositories;
 
 import com.attasportsapp.models.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
+
+    @Query(value = "SELECT DISTINCT l FROM Location l JOIN FETCH l.sports as s WHERE  s.name IN (:sportsNames) AND s.startDate BETWEEN :startDate AND :endDate ORDER BY s.cost")
+    List<Location> findAllBetweenDates(@Param("sportsNames") List<String> sportsNames, @Param("startDate") LocalDate startDate, @Param("endDate")LocalDate endDate);
 }
